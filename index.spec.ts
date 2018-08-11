f503a8e05cbc85afc3cc6c6e496e2c4653a1be50
Leo Singleton <leo@leosingleton.com>
2018-08-11 19:08:17 -0400

    Adding unit test for the new AsyncMutex class

--- index.spec.ts
+++ index.spec.ts
@@ -1,4 +1,5 @@
 import './coordination/AsyncEventWaitHandle.spec';
+import './coordination/AsyncMutex.spec';
 import './coordination/AsyncTimerEvent.spec';
 import './Connection.spec';
 import './ControlFrame.spec';
