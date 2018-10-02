591e4bf63696aca19aea4562bb014e46168cbc03
Leo Singleton <leo@leosingleton.com>
2018-10-01 20:57:42 -0400

    Porting C# changes to TypeScript
    This covers new fields in the protocol for planned features

--- BinaryConverter.ts
+++ BinaryConverter.ts
@@ -3,6 +3,28 @@
  * build our own...
  */
 export class BinaryConverter {
+  public static readUInt64(buffer: Uint8Array, startIndex: number): number {
+    return (buffer[startIndex] << 56) |
+      (buffer[startIndex + 1] << 48) |
+      (buffer[startIndex + 2] << 40) |
+      (buffer[startIndex + 3] << 32) |
+      (buffer[startIndex + 4] << 24) |
+      (buffer[startIndex + 5] << 16) |
+      (buffer[startIndex + 6] << 8) |
+      buffer[startIndex + 7];
+  }
+
+  public static writeUInt64(buffer: Uint8Array, startIndex: number, value: number): void {
+    buffer[startIndex] = (value & 0xff00000000000000) >> 56;
+    buffer[startIndex + 1] = (value & 0xff000000000000) >> 48;
+    buffer[startIndex + 2] = (value & 0xff0000000000) >> 40;
+    buffer[startIndex + 3] = (value & 0xff00000000) >> 32;
+    buffer[startIndex + 4] = (value & 0xff000000) >> 24;
+    buffer[startIndex + 5] = (value & 0xff0000) >> 16;
+    buffer[startIndex + 6] = (value & 0xff00) >> 8;
+    buffer[startIndex + 7] = value & 0xff;
+  }
+
   public static readInt32(buffer: Uint8Array, startIndex: number): number {
     return (buffer[startIndex] << 24) |
       (buffer[startIndex + 1] << 16) |
@@ -23,6 +45,15 @@ export class BinaryConverter {
 
   public static writeUInt16(buffer: Uint8Array, startIndex: number, value: number): void {
     buffer[startIndex] = (value & 0xff00) >>> 8;
+    buffer[startIndex + 1] = value & 0xff;
+  }
+
+  public static readInt16(buffer: Uint8Array, startIndex: number): number {
+    return ((buffer[startIndex] << 8) | buffer[startIndex + 1]);
+  }
+
+  public static writeInt16(buffer: Uint8Array, startIndex: number, value: number): void {
+    buffer[startIndex] = (value & 0xff00) >> 8;
     buffer[startIndex + 1] = value & 0xff;
   }
 }
