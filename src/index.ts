50bb8731eac299f61a5e1f191d5760bb9990be91
Leo Singleton <leo@leosingleton.com>
2020-04-19 17:25:22 -0400

    Adding copyright header

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
+/*!
+ * WebSocketRT: Real-time websocket library
+ * Copyright (c) Leo C. Singleton IV <leo@leosingleton.com>
+ * Released under the MIT license
+ */
+
 import { Connection } from './Connection';
 import { IFramedSocket } from './IFramedSocket';
 import { Message } from './Message';
