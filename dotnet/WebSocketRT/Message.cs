5a3a0e7284fdc05a82081c021b9bd81af03ad6fb
Leo Singleton <leo@leosingleton.com>
2020-09-09 08:57:45 -0400

    Replacing Newtonsoft.Json with System.Text.Json

--- dotnet/WebSocketRT/Message.cs
+++ dotnet/WebSocketRT/Message.cs
@@ -1,7 +1,7 @@
-﻿using Newtonsoft.Json;
-using System;
+﻿using System;
 using System.Diagnostics;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace LeoSingleton.WebSocketRT
@@ -114,16 +114,16 @@ namespace LeoSingleton.WebSocketRT
         public T GetPayloadAsJson<T>()
         {
             var payloadString = Encoding.UTF8.GetString(Payload);
-            return JsonConvert.DeserializeObject<T>(payloadString);
+            return JsonSerializer.Deserialize<T>(payloadString);
         }
 
         /// <summary>
         /// Writes the JSON notation for an object into the payload property
         /// </summary>
         /// <param name="obj">Payload to be encoded as JSON</param>
-        public void SetPayloadAsJson(object obj)
+        public void SetPayloadAsJson<T>(T obj)
         {
-            var payloadString = JsonConvert.SerializeObject(obj);
+            var payloadString = JsonSerializer.Serialize(obj);
             Payload = Encoding.UTF8.GetBytes(payloadString);
         }
 
